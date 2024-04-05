0922f88f459637ebb30292849a06e4d12f14b1dc
jdufresne12 <jdufresne135@gmail.com>
2024-04-04 20:03:02 -0400

    Problem1 complete

--- README.md
+++ README.md
@@ -4,16 +4,18 @@ John Dufresne - COP4520 Assignment 3 - Due 4/5/2024
 ## How to run
 - ### Problem 1
   ```command
-  
+    g++ problem1.cpp -o problem1 -std=c++20
+    ./problem1
   ```
 
 - ### Problem 2
   ```command
-  
+    g++ problem2.cpp -o problem2 -std=c++20
+    ./problem2
   ```
 
 ## Problem 1: The Birthday Presents Party:
-- For my solution to problem 1 I had to take a couple important factors into account. The fact that guests can have multiple cakes, guests cannot communicate with eachother and guests are randomly chosen. I decided to create a vector holding the threads, and assigning the first thread as a counter, to keep track of how many times it replaces the cupcake. With this in mind, guests entering will only be allowed to have one cupcake each, and if they go through the maze and the cupcake isn't there, they cannot call for another one. This allows the counting thread to keep track of how many times it has to recall another cupcake, hence providing the counter with how many of the guests have had a cupcake. When the counter sees that they have counted as many cupcakes as there were total guests. They alert the minotaur that all the guests have been through the maze and have enjoyed the Minotaurs delicious cupcakes. To ensure mutual exclusion each thread has a lock and after the lock is initialized, they wait for the current guest being called to be same as the index of that thread or guest.
+- 
 
 ## Problem 2: Atmospheric Temperature Reading Module:
-- For my solution to problem 2, I decided to choose choice 3 which was the queue. Some advantages of the queue would be the fact that guests can see how long it will take to view the vase through their position in line. Secondly, this avoids the trouble of line skipping ensuring everybody has an equal oportunity to see the vase. Some disadvtanges of this choice however could be the fact that all the guests do have to wait in-line instead of walking around doing other things while they wait. Another disadvantage is how it is a little more difficult to setup since we're dealing with locks and queues. However choice 3 was optimal in my opinion, and it was also a great learning experience dealing with queues, locks and threads. 
+-  
---
+++ problem1.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <thread>
+#include <vector>
+#include <mutex>
+#include <condition_variable>
+#include <random>
+#include <algorithm>
+#include <atomic>
+#include <string>
+#include <chrono>
+#include <fstream>
+
+using namespace std;
+
+const int NUM_SERVANTS = 4;
+const int NUM_GIFTS = 500000;
+
+// Node structure for the linked list
+struct Gift {
+    int tag;
+    Gift* next;
+    Gift(int t) : tag(t), next(nullptr) {}
+};
+
+// Global variables
+mutex mtx;                  // Protects linked list and output
+mutex Gift_mtx;             // Protects access to gifts vector
+condition_variable cv;      // Condition Variable
+Gift* head = nullptr;       // Head of linked list
+Gift* tail = nullptr;       // Tail of linked list
+vector<int> gifts;          // Bag of unordered gifts
+vector<int> addedGifts;     // gifts added to linked list, so we can grab them and delete easily
+atomic<int> giftsCount(0);  // Gift counter used to make sure 
+atomic<int> num_gifts(0);   // Keeps track of number of gifts seen
+atomic<int> num_cards(0);   // Keeps track of number of Thank You cards written
+ofstream outFile("Problem1output.txt");    
+
+// Safely output messages to the console
+void safePrint(const string& message) {
+    static mutex fileMutex; // Protects access to outFile
+    lock_guard<mutex> lock(fileMutex);
+    if(outFile.is_open()) {
+        outFile << message << endl;
+    }
+}
+
+// Function to add a Gift to the linked list
+void addGift(int tag) {
+    unique_lock<mutex> lock(mtx);
+    Gift* newGift = new Gift(tag);
+    bool wasEmpty = (head == nullptr);
+
+    if (!head) {
+        head = tail = newGift;
+    } else {
+        tail->next = newGift;
+        tail = newGift;
+    }
+
+    lock.unlock(); // Manual unlock is not strictly necessary here but added for clarity
+    if (wasEmpty) {
+        cv.notify_one(); // Notify only if the list was empty before adding
+    }
+}
+
+// Function to remove a Gift from the linked list
+bool removeGift(int tag) {
+    unique_lock<mutex> lock(mtx);
+    cv.wait(lock, [&]() { return head != nullptr; });
+
+    Gift* curr = head;
+    Gift* prev = nullptr;
+
+    while (curr && curr->tag != tag) {
+        prev = curr;
+        curr = curr->next;
+    }
+
+    if (!curr) {
+        return false;
+    }
+
+    if (!prev) {
+        head = curr->next;
+    } else {
+        prev->next = curr->next;
+    }
+
+    if (!curr->next) {
+        tail = prev;
+    }
+
+    delete curr;
+    return true;
+}
+
+// Function to check if a Gift is in the linked list
+bool checkGift(int tag) {
+    lock_guard<mutex> lock(mtx);
+
+    Gift* curr = head;
+    while (curr && curr->tag != tag) {
+        curr = curr->next;
+    }
+
+    return curr != nullptr;
+}
+
+// Random number generator
+int getRandomInt(int min, int max){
+    random_device seed;
+    mt19937 rng(seed());
+    uniform_int_distribution<mt19937::result_type> dist(min, max);
+    return dist(rng);
+}
+
+// Servant thread function
+void servantTask(int id) {
+    int action = -1;
+    int tag;
+    while (giftsCount > 0 || addedGifts.size() > 0) {
+        action = (action+1) % 3;
+        {
+            lock_guard<mutex> lock(Gift_mtx); // Protect both gifts and addedGifts with the same mutex
+            if (action == 0 && !gifts.empty()) { // Add a Gift
+                tag = gifts.back();
+                gifts.pop_back();
+                giftsCount--;
+                addedGifts.push_back(tag);
+                addGift(tag);
+                num_gifts++;
+                safePrint("Servant " + to_string(id+1) + ": Added gift with tag " + to_string(tag));
+            } 
+            else if (action == 1 && !addedGifts.empty()) { // Remove a Gift
+                tag = addedGifts.front();
+                addedGifts.erase(addedGifts.begin());
+                if (removeGift(tag)) {
+                    num_cards++;
+                    safePrint("Servant " + to_string(id+1) + ": Thank you card written for tag " + to_string(tag));
+                } else {
+                    safePrint("Servant " + to_string(id+1) + ": Gift with tag " + to_string(tag) + " not found");
+                }
+            } 
+            else {
+                tag = getRandomInt(1,NUM_GIFTS);
+                if (checkGift(tag)) {
+                    safePrint("Servant " + to_string(id+1) + ": Gift with tag " + to_string(tag) + " is in the list");
+                } else {
+                    safePrint("Servant " + to_string(id+1) + ": Gift with tag " + to_string(tag) + " is not in the list");
+                }
+            }
+        }
+    }
+}
+
+int main() {
+    // Initialize gifts vector with unique tags
+    for (int i = 0; i < NUM_GIFTS; i++)
+        gifts.push_back(i+1);
+    shuffle(gifts.begin(), gifts.end(), default_random_engine(random_device{}()));
+    giftsCount = NUM_GIFTS;
+
+
+    cout << "Servants are starting their work..." << endl;
+    auto start_timer = chrono::high_resolution_clock::now(); 
+
+    // Create servant threads
+    vector<thread> servants;
+    for (int i = 0; i < NUM_SERVANTS; i++) {
+        servants.emplace_back(servantTask, i);
+    }
+
+    // Join servant threads
+    for (auto& servant : servants) {
+        servant.join();
+    }
+
+    //End timer, find total, and convert to seconds
+    auto end = chrono::high_resolution_clock::now();
+    chrono::duration<double> duration = end - start_timer;
+
+    cout << "Number of Gifts: " << num_gifts << endl;
+    cout << "Number of Cards: " << num_cards << endl;
+    cout << "Runtime: " << duration.count() << " seconds." << endl;
+    return 0;
+}
